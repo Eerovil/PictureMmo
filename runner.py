3a788ce4430a1e6f43133f8fe17654567a6a2f4e
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-08-08 11:54:45 +0300

    Runner runs code, updating database every minute on second 30

--- runner.py
+++ runner.py
@@ -1,35 +1,46 @@
+import compiler
+import sqlite3
+from time import sleep, time
+from datetime import datetime
+
+
 class Runner(object):
-    def __init__(self, playerData):
+    def __init__(self):
         self.reg = [0, 0]
         self.idata = []
         self.width = 100
         self.height = 100
-        self.playerPos = [[data[1], data[2]] for data in playerData]
-        self.playerData = playerData
+        self.playerData = []
         for x in range(self.width):
             self.idata.append([])
             for y in range(self.height):
                 self.idata[x].append([0, 0, 0])
 
+    def setPlayerData(self, playerData):
+        comp = compiler.PmoToPython()
+        self.playerData = [
+            list(data[:8]) + ["\n".join(comp.parse_text(data[8]))] for data in playerData
+        ]
+
     def movePlayer(self, index, dir, amount):
         dir = dir % 4
         if dir == 0:
-            self.playerPos[index][0] += amount
+            self.playerData[index][1] += amount
         elif dir == 1:
-            self.playerPos[index][1] += amount
+            self.playerData[index][2] += amount
         elif dir == 1:
-            self.playerPos[index][0] -= amount
+            self.playerData[index][1] -= amount
         elif dir == 1:
-            self.playerPos[index][1] -= amount
+            self.playerData[index][2] -= amount
 
-        if (self.playerPos[index][0] < 0):
-            self.playerPos[index][0] = 0
-        if (self.playerPos[index][0] >= self.width):
-            self.playerPos[index][0] = self.width - 1
-        if (self.playerPos[index][1] < 0):
-            self.playerPos[index][1] = 0
-        if (self.playerPos[index][1] >= self.height):
-            self.playerPos[index][1] = self.height - 1
+        if (self.playerData[index][1] < 0):
+            self.playerData[index][1] = 0
+        if (self.playerData[index][1] >= self.width):
+            self.playerData[index][1] = self.width - 1
+        if (self.playerData[index][2] < 0):
+            self.playerData[index][2] = 0
+        if (self.playerData[index][2] >= self.height):
+            self.playerData[index][2] = self.height - 1
 
     def swapReg(self):
         _tmp = self.reg[0]
@@ -37,7 +48,7 @@ class Runner(object):
         self.reg[1] = _tmp
 
     def checkColor(self, index):
-        color = self.idata[self.playerPos[index][0]][self.playerPos[index][1]]
+        color = self.idata[self.playerData[index][1]][self.playerData[index][2]]
         if (color[0] == 0 and color[1] == 0 and color[2] == 0):
             self.reg[0] = 2
         elif (color[0] == self.playerData[index][2] and
@@ -51,7 +62,7 @@ class Runner(object):
         self.reg[0] = value
 
     def drawPlayer(self, index):
-        self.idata[self.playerPos[index][0]][self.playerPos[index][1]] = (
+        self.idata[self.playerData[index][1]][self.playerData[index][2]] = (
             [self.playerData[index][2], self.playerData[index][3], self.playerData[index][4]]
         )
 
@@ -63,21 +74,56 @@ class Runner(object):
 
     def run(self):
         for i in range(len(self.playerData)):
+            self.reg[0] = self.playerData[i][6]
+            self.reg[1] = self.playerData[i][7]
             code = self.playerData[i][8]
             exec(code)
-        print(self.playerPos)
+            self.playerData[i][6] = self.reg[0]
+            self.playerData[i][7] = self.reg[1]
+        print([data[1:3] for data in self.playerData])
+
+
+class Synchronizer(object):
+    def __init__(self):
+        self.runner = Runner()
+        self.tps = 10
+        self.fetch_from_database()
+
+    def loop(self):
+        fetched = False
+        while True:
+            second = datetime.now().second
+            if not fetched and second == 30:
+                self.fetch_from_database()
+                fetched = True
+            elif second < 30:
+                fetched = False
+            frame_start = time()
+            self.runner.run()
+            to_sleep = (1.0 / self.tps) - (time() - frame_start)
+            if to_sleep < 0:
+                print("FRAME TOOK TOO LONG")
+            else:
+                sleep(to_sleep)
+
+    def fetch_from_database(self):
+        with sqlite3.connect('database.db') as conn:
+            c = conn.cursor()
+            # Store positions & registers
+            regdata = []
+            for data in self.runner.playerData:
+                regdata.append([data[1], data[2], data[6], data[7], data[0]])
+            c.executemany(
+                'UPDATE users SET posx = ? , posy = ? , reg0 = ? , reg1 = ? WHERE name = ? ',
+                regdata
+            )
+            self.runner.setPlayerData(c.execute('SELECT * FROM users').fetchall())
+            print("Updated data")
 
 
 if __name__ == "__main__":
-    import sqlite3
-    conn = sqlite3.connect('database.db')
-    import compiler
-
-    comp = compiler.PmoToPython()
-
-    c = conn.cursor()
-    playerData = []
-    for data in c.execute('''SELECT * FROM users''').fetchall():
-        playerData.append(list(data[:-1] + ("\n".join(comp.parse_text(data[-1])), )))
-    r = Runner(playerData)
-    r.run()
+    s = Synchronizer()
+    try:
+        s.loop()
+    except KeyboardInterrupt:
+        pass
---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    name TEXT PRIMARY KEY,
+    posx INT,
+    posy INT,
+    red INT,
+    green INT,
+    blue INT,
+    reg0 INT,
+    reg1 INT,
+    code TEXT
+)
