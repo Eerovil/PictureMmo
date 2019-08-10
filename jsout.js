783aefcbc9e1c13e29427479f055257f6a305ab7
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-08-10 08:26:38 +0300

    ignore

--- jsout.js
+++
@@ -1,6 +0,0 @@
-playerData = [
-['Eero', 10, 89, 255, 0, 0, 16, 0, 'movePlayer(i, getReg(), 1);swapReg();checkColor(i, idata);if (getReg() == 0) {swapReg();addReg(2);movePlayer(i, getReg(), 1);addReg(1);} else {swapReg();}idata = drawPlayer(i, idata);'],
-['Anna', 36, 99, 0, 255, 0, 182, 1, 'idata = drawPlayer(i, idata);movePlayer(i, getReg(), 1);swapReg();checkColor(i, idata);if (getReg() == 0) {swapReg();addReg(1);} else {swapReg();}'],
-['Eer2', 37, 99, 0, 0, 255, 182, 1, 'idata = drawPlayer(i, idata);movePlayer(i, getReg(), 1);swapReg();checkColor(i, idata);if (getReg() == 0) {swapReg();addReg(1);} else {swapReg();}'],
-]
-timestamp = new Date('2019-08-09T17:04:30.067944')
