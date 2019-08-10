0c3efe8db4fa8a74e0445ba8d2cdccc1e718d296
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-08-10 09:41:01 +0300

    Updates

--- baseindex.js
+++ baseindex.js
@@ -16,38 +16,62 @@ var tps = 10,
 window.addEventListener('load', onLoad);
 
 function onLoad() {
-    reloadAll();
     canvas = document.getElementById("mainCanvas");
     ctx = canvas.getContext("2d");
 
-    let tmpcanvas = document.createElement('canvas');
-    tmpctx = tmpcanvas.getContext("2d");
-
-    var img = new Image();
-    img.onload = () => {
-        tmpctx.imageSmoothingEnabled = false;
-        tmpctx.drawImage(img, 0, 0, width, height, 0, 0, width, height);
-        tmpctx.scale(5,5);
-        idata = tmpctx.getImageData(0, 0, width, height);
-        ctx.clearRect(0, 0, width, height);
-        ctx.putImageData(idata, 0, 0);
-
+    reloadAll().then(() => {
         tickLooper(idata);
-    }
-    img.src = 'image.png?cachebuster=' + new Date().getTime();
-
+    });
 };
 
-function reloadAll() {
+async function reloadAll() {
     ticksSinceUpdate = 0;
-    for (oldScript of document.querySelectorAll('head script')) {
-        console.log(oldScript)
-        oldScript.remove();
-    }
-    var head= document.getElementsByTagName('head')[0];
-    var script= document.createElement('script');
-    script.src= 'jsout.js?cachebuster=' + new Date().getTime();
-    head.appendChild(script);
+    timestamp = new Date();
+    
+    let promises = [];
+    promises.push(new Promise((resolve, reject) => {
+        for (oldScript of document.querySelectorAll('head script')) {
+            console.log(oldScript)
+            oldScript.remove();
+        }
+        var head= document.getElementsByTagName('head')[0];
+        var script= document.createElement('script');
+        script.src= 'jsout.js?cachebuster=' + new Date().getTime();
+        head.appendChild(script);
+        script.onload = function() {
+            if (new Date() - timestamp > 60000) {
+                alert("No data from server");
+            }
+            resolve();
+        }
+        script.onerror = reject;
+    }));
+
+    promises.push(new Promise((resolve, reject) => {
+        let tmpcanvas = document.createElement('canvas');
+        tmpctx = tmpcanvas.getContext("2d");
+    
+        var img = new Image();
+        img.onload = () => {
+            tmpctx.drawImage(img, 0, 0, width, height, 0, 0, width, height);
+            idata = tmpctx.getImageData(0, 0, width, height);
+            ctx.clearRect(0, 0, width * 500, height * 500);
+            ctx.imageSmoothingEnabled = false;
+            for (let pos=0; pos<idata.data.length; pos+=4) {
+                if (idata.data[pos  ] != 255 ||
+                    idata.data[pos+1] != 255 ||
+                    idata.data[pos+2]!= 255) {
+                        ctx.fillStyle = `rgb(${idata.data[pos]}, ${idata.data[pos+1]}, ${idata.data[pos+2]})`;
+                        ctx.fillRect(((pos / 4) % width) * 5, (Math.floor((pos / 4) / width)) * 5, 5, 5);
+                    }
+            }
+            resolve();
+        }
+        img.onerror = reject;
+        img.src = 'image.png?cachebuster=' + new Date().getTime();
+    }));
+
+    await Promise.all(promises);
 }
 
 
@@ -55,13 +79,12 @@ function tickLooper(idata) {
     let lastTick = new Date();
     let new_idata = nextImageState(idata);
     ticksSinceUpdate++;
-    ctx.imageSmoothingEnabled = false;
-    ctx.putImageData(new_idata, 0, 0);
+    // ctx.putImageData(new_idata, 0, 0);
 
     const currTime = new Date()
     // Ususally on first load we have "too fresh" data. Need to run slower/faster
-    servertick = (((currTime - timestamp) / 1000) * maxtps) - 30 * maxtps;
-    console.log("servertick", servertick, "late", ticksSinceUpdate - servertick)
+    servertick = Math.floor((((currTime - timestamp) / 1000) * maxtps) - 30 * maxtps);
+    console.log("servertick", servertick, "clienttick", ticksSinceUpdate)
     if (ticksSinceUpdate - servertick < -2) {
         tps = 2 * maxtps;
     } else if (ticksSinceUpdate - servertick > 2) {
@@ -71,7 +94,6 @@ function tickLooper(idata) {
     }
 
     // Reload once we have run all ticks
-    console.log(tps)
     if (ticksSinceUpdate >= maxtps * 60) {
         reloadAll();
     }
@@ -112,12 +134,28 @@ function movePlayer(index, dir, amount) {
     }
 }
 function drawPlayer(index, idata) {
-    let pos = (playerData[index][2] * width + playerData[index][1]) * 4;
+    const xpos = playerData[index][1],
+          ypos = playerData[index][2];
+    playerRect(index, xpos, ypos);
+
+    let pos = (ypos * width + xpos) * 4;
     idata.data[pos  ] = playerData[index][3]
     idata.data[pos+1] = playerData[index][4]
     idata.data[pos+2] = playerData[index][5]
     return idata;
 }
+
+function playerRect(index, xpos, ypos) {
+    ctx.fillStyle = `rgb(${playerData[index][3]}, ${playerData[index][4]}, ${playerData[index][5]})`;
+    ctx.fillRect(xpos * 5, ypos * 5, 5, 5);
+}
+function posRect(index, xpos, ypos) {
+    ctx.fillStyle = `rgb(0, 0, 0)`;
+    ctx.fillRect(xpos * 5, ypos * 5, 5, 5);
+    ctx.fillStyle = `rgb(${playerData[index][3]}, ${playerData[index][4]}, ${playerData[index][5]})`;
+    ctx.fillRect(xpos * 5 + 1, ypos * 5 + 1, 3, 3);
+}
+
 function getReg() {
     return reg[0]
 }
@@ -161,10 +199,16 @@ function subReg(num) {
 
 
 function nextImageState(idata) {
+    ctx.strokeStyle = `rgb(0, 0, 0)`;
+    ctx.lineWidth = "1";
     for (let i=0; i<playerData.length; i++) {
         reg[0] = playerData[i][6];
         reg[1] = playerData[i][7];
+        const prevx = playerData[i][1],
+              prevy = playerData[i][2];
         eval(playerData[i][8]);
+        playerRect(i, prevx, prevy);
+        posRect(i, playerData[i][1], playerData[i][2])
         playerData[i][6] = reg[0];
         playerData[i][7] = reg[1];
         
